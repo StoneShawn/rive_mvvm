fa87ac27ca5c6b1f396182c1ae82fc3a8ca97bc2
shawn <gg720219@gmail.com>
2023-02-16 23:22:12 +0800

    initial commit

---
+++ app/src/main/java/com/shawn/rive_mvvm/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.shawn.rive_mvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shawn.rive_mvvm.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
